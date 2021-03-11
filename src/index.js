47af66e1e84b9d55980f38b1c0121abd39bed9de
TomTylor <tomtylorr@gmail.com>
2021-03-11 22:18:30 +0300

    feat: writed code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
 
 exports.min = function min (array) {
-  return 0;
+  let newArr = array[1];
+
+  if(array === undefined) {
+    return 0;
+  } 
+
+  for (let i = 0, b = 1; i < array.length; i++, b++) {
+    
+
+    if (newArr < array[i]) {
+      newArr = newArr;
+    } else if (newArr > array[i]) {
+      newArr = array[i];
+    } else {
+      newArr = newArr;
+    }
+      
+  }
+
+  return newArr;
 }
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  let newArr = array[1];
+
+  if(array === undefined) {
+    return 0;
+  } 
+  
+  for (let i = 0, b = 1; i < array.length; i++, b++) {
+    
+  
+    if (newArr > array[i]) {
+      newArr = newArr;
+    } else if (newArr < array[i]) {
+      newArr = array[i];
+    } else {
+      newArr = newArr;
+    }
+      
+  }
+  
+  return newArr;
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+  let newArr = 0;
+
+  if(array === undefined) {
+    return 0;
+  } 
+  
+  for (let i = 0; i < array.length; i++) {
+      newArr = newArr + array[i];
+  }
+  
+  return (newArr / (array.length));
 }
+
+
+
+
+
+
